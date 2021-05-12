ffefb0eb17c88a1f4ee8c5c670cb5b69af8199a1
Niewiar <patrykniewiarowski@gmail.com>
2021-05-12 22:29:10 +0200

    Basic shooting

--- UnityProject/Assets/Scripts/Player/ShootCamera.cs
+++ UnityProject/Assets/Scripts/Player/ShootCamera.cs
@@ -7,7 +7,8 @@ public class ShootCamera : CinemachineExtension
     [Space(10)]
     [Tooltip("Szybkość myszki horyzontalnie")] public float horizontalSpeed = 10;
     [Tooltip("Szybkość myszki wertykalnie")] [SerializeField] private float verticalSpeed = 10f;
-    [Tooltip("Zablokowanie myszki wertykalnie")] [SerializeField] private float clampAngle = 80f;
+    [Tooltip("Zablokowanie myszki wertykalnie")] [Range(1, 180)] [SerializeField] private float maxClampAngle = 80f;
+    [Tooltip("Zablokowanie myszki wertykalnie")] [Range(-180, -1)] [SerializeField] private float minClampAngle = -40;
 
     private float turnSmoothVelocity;
     private InputManager inputManager;
@@ -35,7 +36,7 @@ public class ShootCamera : CinemachineExtension
                 Vector2 deltaInput = inputManager.GetMouseDelta();
                 startingRotation.x += deltaInput.x * verticalSpeed * Time.deltaTime;
                 startingRotation.y += deltaInput.y * horizontalSpeed * Time.deltaTime;
-                startingRotation.y = Mathf.Clamp(startingRotation.y, -clampAngle, clampAngle);
+                startingRotation.y = Mathf.Clamp(startingRotation.y, minClampAngle, maxClampAngle);
 
                 state.RawOrientation = Quaternion.Euler(-startingRotation.y, player.eulerAngles.y, 0);
             }
--- UnityProject/Assets/Scripts/Player/Shooting.cs
+++ UnityProject/Assets/Scripts/Player/Shooting.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 [RequireComponent(typeof(Movement))]
 public class Shooting : MonoBehaviour
@@ -8,6 +9,12 @@ public class Shooting : MonoBehaviour
     [SerializeField] private Transform shootPoint;
     [SerializeField] private GameObject bulletPrifab;
     [SerializeField] private GameObject viewfinder;
+    [SerializeField] private Camera mainCam;
+    [SerializeField] private float shootForce = 20f;
+    [SerializeField] private Rig gunRig;
+    [SerializeField] private RigBuilder rigBuilder;
+    [SerializeField] private GameObject gun;
+    [SerializeField] private GameObject gunGFX;
 
     private InputManager inputManager;
     private Movement movement;
@@ -16,25 +23,48 @@ public class Shooting : MonoBehaviour
     {
         movement = GetComponent<Movement>();
         inputManager = InputManager.Instance;
-
+        gunGFX.SetActive(false);
     }
 
     void Update()
     {
         if (movement.playerIsInShootPose)
         {
+            gunGFX.SetActive(true);
+            GunLookAt();
+
+            if (rigBuilder.enabled == false)
+            {
+                rigBuilder.enabled = true;
+            }
+
             viewfinder.SetActive(true);
+            gunRig.weight = 1;
 
             if (inputManager.PlayerShoot())
             {
-                var obj = Instantiate(bulletPrifab);
-                obj.transform.position = shootPoint.position;
-                obj.transform.rotation = shootPoint.rotation;
+                CreateBullet();
             }
         }
         else
         {
+            gunGFX.SetActive(false);
             viewfinder.SetActive(false);
+            gunRig.weight = 0;
         }
+    }
+
+    private void CreateBullet()
+    {
+        var obj = Instantiate(bulletPrifab, shootPoint.position, Quaternion.identity);
+        Rigidbody rb = obj.GetComponent<Rigidbody>();
+        rb.velocity = mainCam.transform.forward * shootForce;
+    }
+
+    private void GunLookAt()
+    {
+        gun.transform.LookAt(new Vector3(Camera.main.ScreenToWorldPoint(viewfinder.transform.position).x,
+            Camera.main.ScreenToWorldPoint(viewfinder.transform.position).y,
+            -Camera.main.ScreenToWorldPoint(viewfinder.transform.position).z));
     }
 }
