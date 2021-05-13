7f4cca31998f7dd03ed6f47a243d5420ea0b0016
Niewiar <patrykniewiarowski@gmail.com>
2021-05-13 15:19:24 +0200

    start scripting debug raport

---
+++ UnityProject/Assets/Scripts/Debug/CSVManager.cs
@@ -0,0 +1,84 @@
+﻿using System.IO;
+using UnityEngine;
+
+public class CSVManager
+{
+    private static string reportFolderName = "Report";
+    private static string reportFileName = "report.csv";
+    private static string reportSeparator = ";";
+    private static string timeStampHeader = "Play date";
+    private static string[] reportHeaders = new string[2] { "Lp.", "Play time" };
+
+    public static void AppendToReport(string[] strings)
+    {
+        VerifyDirectory();
+        VerifyFile();
+
+        using (StreamWriter sw = File.AppendText(GetFilePath()))
+        {
+            string finaleString = "";
+            for (int i = 0; i < strings.Length; i++)
+            {
+                if (finaleString != "")
+                {
+                    finaleString += reportSeparator;
+                }
+                finaleString += strings[i];
+            }
+            finaleString += reportSeparator + GetTimeStemp();
+            sw.WriteLine(finaleString);
+        }
+    }
+
+    public static void CreateReport()
+    {
+        VerifyDirectory();
+        using (StreamWriter sw = File.CreateText(GetFilePath()))
+        {
+            string finaleString = "";
+            for (int i = 0; i < reportHeaders.Length; i++)
+            {
+                if (finaleString != "")
+                {
+                    finaleString += reportSeparator;
+                }
+                finaleString += reportHeaders[i];
+            }
+            finaleString += reportSeparator + timeStampHeader;
+            sw.WriteLine(finaleString);
+        }
+    }
+
+    private static void VerifyDirectory()
+    {
+        string dir = GetDirectoryPath();
+        if (!Directory.Exists(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+    }
+
+    private static void VerifyFile()
+    {
+        string file = GetFilePath();
+        if (!File.Exists(file))
+        {
+            CreateReport();
+        }
+    }
+
+    private static string GetDirectoryPath()
+    {
+        return Application.dataPath + "/" + reportFolderName;
+    }
+
+    private static string GetFilePath()
+    {
+        return GetDirectoryPath() + "/" + reportFileName;
+    }
+
+    private static string GetTimeStemp()
+    {
+        return System.DateTime.Now.ToString();
+    }
+}
---
+++ UnityProject/Assets/Scripts/Debug/DebugToExel.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[InitializeOnLoad]
+public class DebugToExel
+{
+    static DebugToExel()
+    {
+        EditorApplication.playModeStateChanged += UpdateReport;
+    }
+
+    [MenuItem("Debug/Reset report %F1")]
+    private static void ResetReport()
+    {
+        CSVManager.CreateReport();
+        Debug.Log("<color=orange>The report has been reset!</color>");
+    }
+
+    private static void UpdateReport(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingPlayMode)
+        {
+            CSVManager.AppendToReport(new string[] { "1", "666" });
+            Debug.Log("<color=green>Report updated!</color>");
+        }
+    }
+}
