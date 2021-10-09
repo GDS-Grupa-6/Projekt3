b876dc41757b3f2f7fafe35e0e4b4004e2ee9910
Niewiar <patrykniewiarowski@gmail.com>
2021-10-09 18:34:09 +0200

    Prepare for audio

---
+++ Raven/Assets/_Scripts/Core/Audio/AudioManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager
+{
+    private AudioReferences _references;
+
+    public AudioManager(AudioReferences p_references)
+    {
+        _references = p_references;
+    }
+}
