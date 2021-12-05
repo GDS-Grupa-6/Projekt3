cbd666a2e871aa6c21acb1c7669a95aa1b905b21
darkezowsky <darkezowsky@gmail.com>
2021-12-05 16:11:45 +0100

    added settings menu (not scripted yet)

---
+++ Raven/Assets/SettingsMenu.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    
+    public void SetVolume (float volume)
+    {
+        Debug.Log(volume);
+    }
+
+}
